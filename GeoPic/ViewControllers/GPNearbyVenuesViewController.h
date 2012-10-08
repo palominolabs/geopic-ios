2ca050f9389aad414380d599da27d03c948c9f6d
Manuel Wudka-Robles <mwudka@me.com>
2012-10-08 14:21:27 -0700

    Created empty view controller for nearby places

---
+++ GeoPic/ViewControllers/GPNearbyVenuesViewController.h
@@ -0,0 +1,13 @@
+//
+//  GPNearbyVenuesViewController.h
+//  GeoPic
+//
+//  Created by Manuel Wudka-Robles on 10/8/12.
+//  Copyright (c) 2012 Palomino Labs, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GPNearbyVenuesViewController : UIViewController
+
+@end
---
+++ GeoPic/ViewControllers/GPNearbyVenuesViewController.m
@@ -0,0 +1,19 @@
+//
+//  GPNearbyVenuesViewController.m
+//  GeoPic
+//
+//  Created by Manuel Wudka-Robles on 10/8/12.
+//  Copyright (c) 2012 Palomino Labs, Inc. All rights reserved.
+//
+
+#import "GPNearbyVenuesViewController.h"
+
+@interface GPNearbyVenuesViewController ()
+
+@end
+
+@implementation GPNearbyVenuesViewController
+
+
+
+@end
