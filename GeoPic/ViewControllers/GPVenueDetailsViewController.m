cc62d71c57330edfbb00c225ae2bd54a370e0600
Manuel Wudka-Robles <mwudka@me.com>
2012-10-10 11:39:45 -0700

    Display venue details in VenueDetails ViewController

--- GeoPic/ViewControllers/GPVenueDetailsViewController.m
+++ GeoPic/ViewControllers/GPVenueDetailsViewController.m
@@ -13,6 +13,8 @@
 
 @end
 
+
+
 @implementation GPVenueDetailsViewController {
     GPVenue *_venue;
 }
@@ -22,10 +24,45 @@
         _venue = venue;
         
         self.navigationItem.title = _venue.title;
-        
-        DLOG(@"Viewing details for venue %@", _venue.title);
     }
     return self;
 }
+
+#pragma mark UITableViewDataSource
+
+- (BOOL)tableView:(UITableView *)tableView shouldHighlightRowAtIndexPath:(NSIndexPath *)indexPath {
+    return NO;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 3;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    static NSString *kCellReuseIdentifier = @"cell";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:kCellReuseIdentifier];
+    if (!cell) {
+        cell = [[[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue2 reuseIdentifier:kCellReuseIdentifier] autorelease];
+    }
+    
+    switch (indexPath.row) {
+        case 0:
+            cell.textLabel.text = NSLocalizedString(@"Category", @"Category");
+            cell.detailTextLabel.text = _venue.category;
+            break;
+        case 1:
+            cell.textLabel.text = NSLocalizedString(@"Checkins", @"Checkins");
+            cell.detailTextLabel.text = [NSString stringWithFormat:@"%d", _venue.checkinsCount];
+            break;
+        case 2:
+            cell.textLabel.text = NSLocalizedString(@"Phone", @"Phone");
+            cell.detailTextLabel.text = _venue.formattedPhone;
+            break;
+    }
+    
+    return cell;
+}
+
 
 @end
